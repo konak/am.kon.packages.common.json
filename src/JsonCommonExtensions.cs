bdb6a21ba092bddbae8ba76ab7f8788a7ae079dc
sergey konakov <konakov.sergey@gmail.com>
2023-10-24 19:16:59 +0400

    1. Switch from Newtonsoft serializer to System.Text.Json 2. Add ability to configure Default serialization settings 3. Update some comments 4. Add null objects handling 5. Add possibility to provide custom serialization options to methods: ToObject and ToJson

--- src/JsonCommonExtensions.cs
+++ src/JsonCommonExtensions.cs
@@ -1,46 +1,80 @@
 ﻿using System;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
+using System.Text.Json;
 
-namespace am.kon.packages.common.json
+/// <summary>
+/// Provides common JSON serialization and deserialization extensions.
+/// </summary>
+public static class JsonCommonExtensions
 {
     /// <summary>
-    /// Common extesion to be used for Json serialize / deserialize
+    /// Common settings to be used during serialization and deserialization.
     /// </summary>
-    public static class JsonCommonExtensions
+    public static JsonSerializerOptions DefaultSerializerOptions { get; private set; }
+
+    /// <summary>
+    /// Static constructor to initialize default serializer options.
+    /// </summary>
+    static JsonCommonExtensions()
     {
-        /// <summary>
-        /// Common settings to be used during serialize / deserialize process
-        /// </summary>
-        public static readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings()
+        DefaultSerializerOptions = new JsonSerializerOptions
         {
-            Formatting = Formatting.None,
-            ContractResolver = new DefaultContractResolver
-            {
-                NamingStrategy = new CamelCaseNamingStrategy()
-            }
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = false,
         };
+    }
 
-        /// <summary>
-        /// Extension method to serialise any type of the object into Json formated string
-        /// </summary>
-        /// <param name="item">Instance of an object to be serialized</param>
-        /// <returns>Json formated string implementation of the provided object</returns>
-        public static string ToJson(this object item)
-        {
-            return JsonConvert.SerializeObject(item, SerializerSettings);
-        }
+    /// <summary>
+    /// Configures the default serializer options.
+    /// </summary>
+    /// <param name="options">The custom serializer options.</param>
+    /// <remarks>
+    /// This method allows for custom configuration of serialization options.
+    /// Example:
+    /// <code>
+    /// var options = new JsonSerializerOptions { WriteIndented = true };
+    /// JsonCommonExtensions.Configure(options);
+    /// </code>
+    /// </remarks>
+    /// <exception cref="ArgumentNullException">Thrown when the provided options are null.</exception>
+    public static void Configure(JsonSerializerOptions options)
+    {
+        if (options == null) throw new ArgumentNullException(nameof(options));
 
-        /// <summary>
-        /// Extesion method to deserialize Jsonn formated string into an instance of an object
-        /// </summary>
-        /// <typeparam name="T">Type of an object to be deserialized</typeparam>
-        /// <param name="jsonString">Json formated string to be used for deserialization</param>
-        /// <returns>Returns an instance of an object of provided type</returns>
-        public static T ToObject<T>(this string jsonString)
-        {
-            return JsonConvert.DeserializeObject<T>(jsonString, SerializerSettings);
-        }
+        DefaultSerializerOptions = options;
+    }
+
+    /// <summary>
+    /// Serializes the specified object to a JSON string.
+    /// </summary>
+    /// <remarks>
+    /// Uses <see cref="DefaultSerializerOptions"/> if no <paramref name="serializerOptions"/> are provided.
+    /// </remarks>
+    /// <param name="item">The object to serialize.</param>
+    /// <param name="serializerOptions">Options to be used during serialization</param>
+    /// <returns>A JSON string representation of the object.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the provided item is null.</exception>
+    public static string ToJson(this object item, JsonSerializerOptions serializerOptions = null)
+    {
+        if (item == null) throw new ArgumentNullException(nameof(item));
+
+        return JsonSerializer.Serialize(item, serializerOptions ?? DefaultSerializerOptions);
+    }
+
+    /// <summary>
+    /// Deserializes the specified JSON string to an object of type T.
+    /// </summary>
+    /// <remarks>
+    /// Uses <see cref="DefaultSerializerOptions"/> if no <paramref name="serializerOptions"/> are provided.
+    /// </remarks>
+    /// <typeparam name="T">The type of object to deserialize to.</typeparam>
+    /// <param name="jsonString">The JSON string to deserialize.</param>
+    /// <param name="serializerOptions">Options to be used during serialization</param>
+    /// <returns>An object of type T deserialized from the JSON string.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the provided JSON string is null.</exception>
+    public static T ToObject<T>(this string jsonString, JsonSerializerOptions serializerOptions = null)
+    {
+        if (jsonString == null) throw new ArgumentNullException(nameof(jsonString));
+
+        return JsonSerializer.Deserialize<T>(jsonString, serializerOptions ?? DefaultSerializerOptions);
     }
 }
-
